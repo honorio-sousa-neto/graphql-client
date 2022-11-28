af3d88a99f3dd213146b992d52fea3bc2812dd21
Honório Neto <honoriossneto@gmail.com>
2022-11-28 23:19:54 +0100

    tabela criada

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,28 @@
 <template>
-  <p v-if="error">Something went wrong...</p>
-  <p v-if="loading">Loading...</p>
-  <p v-else v-for="(client, index) in result.clients" :key="index">
-    {{ client.name }}
-  </p>
-  <div></div>
+  <div class="wrapper">
+    <p v-if="error">Something went wrong...</p>
+    <p v-if="loading">Loading...</p>
+    <table v-else>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Email</th>
+          <th>Telefone</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="client in result.clients" :key="client.id">
+          <td>{{ client.name }}</td>
+          <td>{{ client.email }}</td>
+          <td>{{ client.phone }}</td>
+          <td class="action">
+            <i class="uil uil-home"></i>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -14,8 +32,10 @@ import { useQuery } from "@vue/apollo-composable";
 const GET_CLIENTS = gql`
   query getClients {
     clients {
+      id
       name
       email
+      phone
     }
   }
 `;
@@ -32,3 +52,26 @@ export default {
   },
 };
 </script>
+<style scoped>
+.wrapper {
+  width: 769px;
+  margin: 0 auto;
+  padding: 1rem;
+  background: rgba(51, 51, 51, 0.123);
+}
+
+tr {
+  display: flex;
+  justify-content: space-between;
+  text-align: left;
+  border-bottom: 1px solid #fff;
+}
+table {
+  width: 100%;
+}
+
+th,
+tbody tr {
+  padding: 0.75rem 1rem;
+}
+</style>
